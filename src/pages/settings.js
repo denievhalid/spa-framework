81157092a9ae94000d69add2857a3b17d1b9487e
Halid <denievhalid88@mail.ru>
2022-08-09 16:57:12 +0300

    commit

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -4,6 +4,13 @@ function Settings() {
   return (
     <div className="page">
       <h1>Settings page</h1>
+      <div className="container">
+        <ul>
+          <li>
+            <a href="/">hello</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
